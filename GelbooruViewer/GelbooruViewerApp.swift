c81355bfc2587fcdcfb6a465138d08b68b5574dc
Amrit Bhogal <ambhogal01@gmail.com>
2024-05-27 01:05:26 -0700

    Initial Commit

---
+++ GelbooruViewer/GelbooruViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GelbooruViewerApp.swift
+//  GelbooruViewer
+//
+//  Created by Amrit Bhogal on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GelbooruViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
